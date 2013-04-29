e48d5c6a691376fbfe083b2fe29b556fc6ed5136
David Duffett <duffett.david@gmail.com>
2013-04-29 17:46:05 +0100

    DateTime was ignoring UTC 'Z' component

--- SolrNet/Impl/FieldParsers/DateTimeFieldParser.cs
+++ SolrNet/Impl/FieldParsers/DateTimeFieldParser.cs
@@ -15,9 +15,7 @@
 #endregion
 
 using System;
-using System.Globalization;
 using System.Linq;
-using System.Xml;
 using System.Xml.Linq;
 
 namespace SolrNet.Impl.FieldParsers {
@@ -39,17 +37,8 @@ namespace SolrNet.Impl.FieldParsers {
 
         public static DateTime ParseDate(string s) {
             var p = s.Split('-');
-            s = p[0].PadLeft(4, '0') + '-' + string.Join("-", p.Skip(1).ToArray());          
-            
-            // Mono does not support that exact format string for some reason, however Parse appears to properly handle the input. 
-            // Try using the format string, and if that fails, fall back to just a niave Parse.
-            DateTime result;
-            if (!DateTime.TryParseExact(s, "yyyy-MM-dd'T'HH:mm:ss.FFF'Z'", CultureInfo.InvariantCulture, DateTimeStyles.None, out result))
-            {
-                result = DateTime.Parse(s, CultureInfo.InvariantCulture);
-            }
-            
-            return result;
+            s = p[0].PadLeft(4, '0') + '-' + string.Join("-", p.Skip(1).ToArray());
+            return DateTime.Parse(s);
         }
     }
 }
